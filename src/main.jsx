e5917b05d99efb9b6ec46d9323bf2013acb55c15
calgsandu <151972731+calgsandu@users.noreply.github.com>
2025-08-06 11:44:26 +0300

    Initial commit: usePopcorn app

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    {/* <StarRating maxRating={5}/>
+    <StarRating maxRating={10}/>
+    <StarRating/> */}
+  </StrictMode>
+);
